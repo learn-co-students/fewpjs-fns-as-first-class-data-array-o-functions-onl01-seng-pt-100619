b7038be67db5c244d78f1ed74b0affed9a8c45d1
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-04-27 16:00:28 +0200

    functions defined independantly then, added into array without parameters, and return text instead of console.log

---
+++ index.js
@@ -0,0 +1,19 @@
+  function wakeDog(dogName, dogBreed) {
+    return `Wake ${dogName} the ${dogBreed}`;
+  }
+  function leashDog(dogName, dogBreed) {
+    return `Leash ${dogName} the ${dogBreed}`;
+  }
+  function walkToPark  (dogName, dogBreed) {
+    return `Walk to the park with ${dogName} the ${dogBreed}`;
+  }
+  function throwFrisbee(dogName, dogBreed) {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  }
+  function walkHome(dogName, dogBreed) {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  }
+  function unleashDog(dogName, dogBreed) {
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+  }
+ let routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog ]
